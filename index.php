fca2966b0fd2f0ad14d3d7744ee0e540f2ce8b60
rayan <rayanaghmer072@outlook.com>
2024-03-26 17:56:48 +0100

    versie1

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Whatsapp 1:1</title>
+</head>
+<body>
+<div class="wrapper">
+    <section class="form signup">
+        <form action="#" method="post" enctype="multipart/form-data" autocomplete = "off">
+            <div class="error-text"></div>
+            <div class="name-details">
+            <div class="field input"> </div>
+            <label>First Name</label>
+            <input type="text" name="fname"
+            placeholder="First Name" required>
+
+            </div>
+    </form>
+    </section>
+</div>
+</body>
+</html>
