7703c3453d29ffd9423b0bd171779472bf3add2f
rayan <rayanaghmer072@outlook.com>
2024-03-28 00:54:39 +0100

    nieuweversie

--- index.php
+++ index.php
@@ -11,13 +11,34 @@
         <form action="#" method="post" enctype="multipart/form-data" autocomplete = "off">
             <div class="error-text"></div>
             <div class="name-details">
-            <div class="field input"> </div>
-            <label>First Name</label>
-            <input type="text" name="fname"
-            placeholder="First Name" required>
+                <div class="field input"> </div>
+                <label>Voornaam</label>
+                <input type="text" name="fname"
+                placeholder="Voornaam" required>
 
             </div>
+            <div class="name-details">
+                <div class="field input"> </div>
+                <label>Achternaam</label>
+                <input type="text" name="fname"
+                placeholder="Achternaam" required>
+            </div>
+
+            <div class= "field input"> 
+            <label>Email Adres</label>
+            <input type="text" name="email" placeholder="Voer je Email In" required>
+            </div>
+
+            <div class="field input">
+                <label>Wachtwoord</label>
+                <input type="password" name="password" placeholder="Voer Nieuw Wachtwoord in" required>
+              <div class="field image">
+                <label>Profiel Foto</label>
+                <input type="file" name="image" accept="image/x-png,image/gif,image/jpeg,image/jpg" required>
+                <div class="field button">
+                <input type="submit" name="submit" value="Doorgaan Naar Chat">
     </form>
+    <div class="link">Heb je al een account? <a href="login.php">Log Nu In</div>
     </section>
 </div>
 </body>
